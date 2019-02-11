bc2417da79958207c4b99ee892d144d01fd6e2aa
Mike Zhou <wangfei@Mikes-Air.hitronhub.home>
2019-02-10 18:27:49 -0800

    Implemented the template with initial pages

---
+++ app/assets/javascripts/star.js
@@ -0,0 +1,19 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+
+//= require jquery
+//= require jquery_ujs
+//= require plugins/jquery/jquery-migrate-1.2.1.min
+//= require jquery-ui
+//= require bootstrap-sprockets
+//= require plugins/selectbox/jquery.selectbox-0.1.3.min
+//= require plugins/countdown/jquery.syotimer
+//= require plugins/datepicker/bootstrap-datepicker.js
+//= require plugins/jquery/waypoints.min
+//= require plugins/counter-up/jquery.counterup.min
+//= require plugins/isotope/isotope.min
+//= require plugins/isotope/jquery.fancybox.pack
+//= require plugins/isotope/isotope-triger
+//= require plugins/slick/slick
+//= require star-custom
