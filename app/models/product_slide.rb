c86841085ae6325e1c6d76e9eb464e6a7639bcae
Mike Zhou <wangfei@Mikes-Air.hitronhub.home>
2019-03-24 14:18:25 -0700

    force slide size

--- app/models/product_slide.rb
+++ app/models/product_slide.rb
@@ -2,6 +2,6 @@ class ProductSlide < ApplicationRecord
 
   belongs_to :product
 
-  has_attached_file :image, styles: { large: "1140x740>", thumb: "100x100>", exact: "1140x740#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "1140x740#", thumb: "100x100>", exact: "1140x740#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
